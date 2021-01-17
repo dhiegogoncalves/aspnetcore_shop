9e33e5049db940bb8ef7dc96fede8ed70ef5af38
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2021-01-16 21:48:37 -0400

    Configuração do Entity Framework Core e implementação da Category Controller.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using aspnetcore_shop.Data;
+using aspnetcore_shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore_shop.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+        {
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
+        {
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync();
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar a categoria" });
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
+        {
+            if (id != model.Id)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Entry<Category>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível atualizar a categoria" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult> Delete(int id, [FromServices] DataContext context)
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            try
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível remover a categoria" });
+            }
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+using aspnetcore_shop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore_shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
